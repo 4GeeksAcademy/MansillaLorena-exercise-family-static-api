bcfae6212b8e54f8200d11462cd840409d08203b
MansillaLorena <lorenamansillag@gmail.com>
2024-11-13 00:52:25 +0000

    first commit

--- src/app.py
+++ src/app.py
@@ -33,10 +33,46 @@ def handle_hello():
     response_body = {
         "hello": "world",
         "family": members
+
     }
 
+    return jsonify(members), 200
 
-    return jsonify(response_body), 200
+@app.route('/member',methods=['POST'])
+def post_member():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({'msg':'Envía la información'}),400
+    if 'first_name' not in body:
+        return  jsonify({'msg':'El campo first_name es obligatorio'}),400
+    if 'age' not in body:
+        return jsonify({'msg':'El campo age es obligatorio'}),400
+    if 'lucky_numbers' not in body:
+        return jsonify({'msg':'El campo lucky_numbers es obligatorio'}),400
+    new_member={
+            'id': body['id'],
+            'first_name': body['first_name'],
+            'last_name': jackson_family.last_name,
+            'age':body['age'],
+            'lucky_numbers':body['lucky_numbers']
+    }
+    member=jackson_family.add_member(new_member)
+    return jsonify({'msg':'OK','member':member})
+@app.route('/member/<int:id_member>',methods=['DELETE'])
+def delete_member(id_member):
+    members_delete=jackson_family.get_member(id_member)
+    if not members_delete:
+        return jsonify({'done':False}),400
+    else:
+        jackson_family.delete_member(id_member)
+        return jsonify({'done':True}),200
+    
+@app.route('/member/<int:id_member>',methods=['GET'])
+def get_one_member(id_member):
+    member=jackson_family.get_member(id_member)
+    if not member:
+        return jsonify({'msg':'No existe miembro con id indicado'}),400
+    return jsonify(member),200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
